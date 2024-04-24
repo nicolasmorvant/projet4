3491599a33d0ff30126f8903bf0d4356e4b3029f
nicolasmorvant <nicolas.morvant@tuta.io>
2024-04-24 15:09:22 +0200

    Test deploiement Github Pages

--- assets/scripts.js
+++ assets/scripts.js
@@ -13,4 +13,16 @@ $(document).ready(function()
         showTags: true,
         tagsPosition: 'top'
     });
+
+
+    //AJOUT DE LA MAP LEAFLET
+    let map = L.map('map').setView([44.846673,-0.60658], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', 
+    {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    let marker = L.marker([44.846673,-0.60658]).addTo(map);
 });
